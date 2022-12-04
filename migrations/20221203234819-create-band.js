135c2a8e8bc3783e26f9547204972d5253d7e65b
George Bastawros <george.bastawros@outlook.com>
2022-12-03 18:15:47 -0600

    migration

--- migrations/20221203234819-create-band.js
+++ migrations/20221203234819-create-band.js
@@ -2,26 +2,27 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bands', {
-      id: {
+    await queryInterface.createTable('bands', {
+      band_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      band_id: {
-        type: Sequelize.INTEGER
-      },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       genre: {
+        allowNull: false,
         type: Sequelize.TEXT
       },
       available_start_time: {
+        allowNull: false,
         type: Sequelize.DATE
       },
       end_time: {
+        allowNull: false,
         type: Sequelize.DATE
       },
       createdAt: {
@@ -35,6 +36,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bands');
+    await queryInterface.dropTable('bands');
   }
 };
